ee2fd26f966d8894a11144b88f13a272318905e3
Fernando Ortiz Rico Celio <fernando_oc96@hotmail.com>
2018-07-07 17:07:48 -0500

    Initial Commit

---
+++ musicApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  musicApp
+//
+//  Created by Fernando Ortiz Rico Celio on 7/7/18.
+//  Copyright © 2018 Fernando Ortiz Rico Celio. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
